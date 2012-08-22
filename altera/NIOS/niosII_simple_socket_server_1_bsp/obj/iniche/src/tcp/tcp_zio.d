fb79f87c469118a9e51de03926e93110faebd606
xavi <xaviertisaire@gmail.com>
2012-08-22 01:05:32 +0000

    tcp funcionant, s'han afegit vectors de sin per a simulació

--- altera/NIOS/niosII_simple_socket_server_1_bsp/obj/iniche/src/tcp/tcp_zio.d
+++ altera/NIOS/niosII_simple_socket_server_1_bsp/obj/iniche/src/tcp/tcp_zio.d
@@ -5,7 +5,13 @@ obj/iniche/src/tcp/tcp_zio.o: iniche/src/tcp/tcp_zio.c \
   HAL/inc/sys/alt_alarm.h HAL/inc/sys/alt_llist.h \
   HAL/inc/priv/alt_alarm.h HAL/inc/alt_types.h system.h \
   UCOSII/inc/ucos_ii.h UCOSII/inc/os_cfg.h HAL/inc/os_cpu.h \
-  iniche/src/h/tk_crnos.h iniche/src/h/libport.h
+  iniche/src/h/tk_crnos.h iniche/src/h/libport.h iniche/src/h/tcpport.h \
+  iniche/src/h/libport.h iniche/src/h/q.h iniche/src/h/netbuf.h \
+  iniche/src/h/net.h iniche/src/h/profiler.h iniche/src/h/ip.h \
+  iniche/src/h/nptcp.h iniche/src/h/mbuf.h iniche/src/h/socket.h \
+  iniche/src/h/sockvar.h iniche/src/h/sockcall.h iniche/src/h/bsdsock.h \
+  iniche/src/h/tcpport.h iniche/src/tcp/protosw.h iniche/src/tcp/in_pcb.h \
+  iniche/src/h/tcp.h iniche/src/tcp/tcp_timr.h iniche/src/tcp/tcp_var.h
 
 iniche/src/h/nios2/ipport.h:
 
@@ -48,3 +54,41 @@ HAL/inc/os_cpu.h:
 iniche/src/h/tk_crnos.h:
 
 iniche/src/h/libport.h:
+
+iniche/src/h/tcpport.h:
+
+iniche/src/h/libport.h:
+
+iniche/src/h/q.h:
+
+iniche/src/h/netbuf.h:
+
+iniche/src/h/net.h:
+
+iniche/src/h/profiler.h:
+
+iniche/src/h/ip.h:
+
+iniche/src/h/nptcp.h:
+
+iniche/src/h/mbuf.h:
+
+iniche/src/h/socket.h:
+
+iniche/src/h/sockvar.h:
+
+iniche/src/h/sockcall.h:
+
+iniche/src/h/bsdsock.h:
+
+iniche/src/h/tcpport.h:
+
+iniche/src/tcp/protosw.h:
+
+iniche/src/tcp/in_pcb.h:
+
+iniche/src/h/tcp.h:
+
+iniche/src/tcp/tcp_timr.h:
+
+iniche/src/tcp/tcp_var.h:
